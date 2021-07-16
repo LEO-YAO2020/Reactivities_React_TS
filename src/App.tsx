1144689a5dc66bd59db32d7a061aa74780f9f487
LeoYao <leoyao98@gmail.com>
2021-07-16 16:39:30 +1200

    initial submit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Axios from 'axios';
+import { Header,List } from 'semantic-ui-react'
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
+  const [activities,setActivities] =  useState([])
+  useEffect(()=>{
+    Axios.get('http://localhost:5000/api/activities').then(
+      res=>{
+        setActivities(res.data)
+      }
+    )
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header as="h2" icon='users' content='Reactivities'/>
+      <List>
+        {activities.map((act:any)=>(
+          <List.Item key={act.id}>
+            {act.title}
+          </List.Item>
+        ))}
+      </List>
     </div>
   );
 }
