44c2ea1667c9a0b978e7bd868ca8dc620b5ed7ac
LeoYao <leoyao98@gmail.com>
2021-11-25 16:05:35 +1300

    singalR Added

--- src/features/activities/details/ActivityDetails.tsx
+++ src/features/activities/details/ActivityDetails.tsx
@@ -4,29 +4,30 @@ import { useParams } from 'react-router-dom'
 import { Grid } from 'semantic-ui-react'
 import LoadingComponent from '../../../app/Layout/LoadingComponent'
 import { useStore } from '../../../app/stores/store'
-import ActicvityDetailedInfo from './ActicvityDetailedInfo'
-import { ActivityDetailedChat } from './ActivityDetailedChat'
+import ActivityDetailedInfo from './ActicvityDetailedInfo'
+import ActivityDetailedChat from './ActivityDetailedChat'
 import ActivityDetailedSidebar from './ActivityDetailedSidebar'
 import ActivityDetailHeader from './ActivityDetailHeader'
 
 const ActivityDetails: React.FC = () => {
   const { activityStore } = useStore()
-  const { selectedActivity: activity, loadActivity, loadingInitial } = activityStore
+  const { selectedActivity: activity, loadActivity, loadingInitial, clearSelectedActivity } = activityStore
   const { id } = useParams<{ id: string }>()
 
   useEffect(() => {
     if (id) loadActivity(id)
-  }, [id, loadActivity])
+    return () => clearSelectedActivity()
+  }, [clearSelectedActivity, id, loadActivity])
 
   if (loadingInitial || !activity) {
     return <LoadingComponent />
   }
   return (
     <Grid>
-      <Grid.Column width={10} >
+      <Grid.Column width={10}>
         <ActivityDetailHeader activity={activity} />
-        <ActicvityDetailedInfo activity={activity}/>
-        <ActivityDetailedChat/>
+        <ActivityDetailedInfo activity={activity} />
+        <ActivityDetailedChat activityId={activity.id} />
       </Grid.Column>
       <Grid.Column width={6}>
         <ActivityDetailedSidebar activity={activity} />
